ec0e0078b313b5452ba6c699e9f26a7a640e8bd7
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-05-12 22:50:33 +0200

    Lecture Basic Operators done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -50,6 +50,8 @@ console.log(typeof year);
 console.log(typeof null);
 */
 
+/*
+// Let, const and var
 let age = 30;
 age = 31;
 
@@ -60,3 +62,31 @@ const birthYear = 1991;
 
 var job = "programmer";
 job = "teacher";
+*/
+
+
+// Basic Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+
+const firstName = "Darko";
+const lastName = "Bogatinov";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10; // x = x + 10;
+x *= 4; // x = x * 4;
+x++; // x = x + 1;
+x--; // x = x - 1;
+x /= 2; // x = x / 2;
+console.log(x);
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
