a2440645e13d3542c47e72a495a5e69deac4f3ce
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-05-12 16:25:25 +0200

    assignment Data Types done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,17 @@
+// assignment Values and Variables
 let country = "Macedonia";
 let continent = "Europe";
 let population = 2.1;
 
 console.log("Country: " + country);
 console.log("Continent: " + continent);
-console.log("Population :" + population + " milion");
+console.log("Population: " + population + " milion");
+
+// assignment Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
