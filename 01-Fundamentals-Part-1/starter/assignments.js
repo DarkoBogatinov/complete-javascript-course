5bd4c0ff05dcc410ae9d5f1b0c4cdd58c04f44bd
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-05-12 15:48:39 +0200

    first assignment done

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Macedonia";
+let continent = "Europe";
+let population = 2.1;
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population :" + population + " milion");
