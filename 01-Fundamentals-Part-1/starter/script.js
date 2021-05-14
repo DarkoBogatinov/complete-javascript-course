f707ee77cc279c7221d0c4c800cdfafea9f80184
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-05-14 13:30:37 +0200

    Lecture Operator Precedence done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -64,7 +64,7 @@ var job = "programmer";
 job = "teacher";
 */
 
-
+/*
 // Basic Operators
 const now = 2037;
 const ageJonas = now - 1991;
@@ -90,3 +90,18 @@ console.log(ageJonas > ageSarah); // >, <, >=, <=
 console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
+*/
+
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
