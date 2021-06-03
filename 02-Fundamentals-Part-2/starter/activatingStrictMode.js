b0c69c966fa013f670c69b5eb4d47574945205c2
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-06-03 15:51:30 +0200

    Activating Strict Mode done

---
+++ 02-Fundamentals-Part-2/starter/activatingStrictMode.js
@@ -0,0 +1,11 @@
+'use strict'; //must be the first line of code
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drige :D');
+
+// const interface = `Audio`;
+// const private = 534;
+// const if = 23;
