d7520b41ea9a470cb77f884b800c579c2244d749
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-06-16 13:15:38 +0200

    Lecture and Assignment Introduction to Arrays done

---
+++ 02-Fundamentals-Part-2/starter/introductionToArrays.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice'];
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3);
+
+const ages = [age1, age2, calcAge(years[2]), calcAge(years[3]), age3];
+console.log(ages);
+
+// Assignment
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const populations = new Array(2.1, 5.3, 5.5, 83);
+console.log(populations.length === 4);
+const percentages = [
+    percentageOfWorld1(populations[0]),
+    percentageOfWorld1(populations[1]),
+    percentageOfWorld1(populations[2]),
+    percentageOfWorld1(populations[3])
+];
+console.log(percentages);
