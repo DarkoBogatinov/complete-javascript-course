4eddd773e96ee4261b340c9f3b5c02e9a1695234
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-06-20 21:45:12 +0200

    Lecture and assignment Introduction to Objects DONE

---
+++ 02-Fundamentals-Part-2/starter/introductionToObjects.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Lecture
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+
+// Assignment
+
+const myCountry = {
+    country: 'Macedonia',
+    capital: 'Skopje',
+    language: 'macedonian',
+    population: 2.1,
+    neighbours: ['Serbia', 'Bulgaria', 'Greece', 'Albania']
+};
