1e9ebbcef7379ee8ab2621f98760f56f1afcf06a
Darko Bogatinov <darko.bogatinov15@gmail.com>
2021-06-21 15:34:04 +0200

    Lecture and Assignment Iteration: The for Loop DONE

---
+++ 02-Fundamentals-Part-2/starter/iterationTheForLoop.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Lecture
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+
+for (let rep = 1; rep <= 30; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+// Assignment
+
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting`);
+}
